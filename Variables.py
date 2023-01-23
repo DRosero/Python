6b660718a3cdf84d08c0667654574b375080d820
DRosero <die_go_ja_vier@yahoo.com>
2023-01-23 15:46:49 -0500

    Operadores

---
+++ Variables.py
@@ -0,0 +1,38 @@
+"""Manejo de enteros"""
+"""print("Esto es una suma")
+numero_uno=2
+numero_dos=5
+resultado=numero_uno+numero_dos
+print(resultado)"""
+
+"""Manejo de strings"""
+"""cadena="Diego"
+cadena+=" "
+cadena+="Rosero"
+print("Esto es una cadena contatenada: "+cadena )"""
+
+"""Manejo de strings y enteros concatenados"""
+"""numero_uno=2
+numero_dos=4
+resultado=numero_uno+numero_dos
+print("El resultado de la suma es: "+str(resultado))"""
+
+"""Busqueda de subcadena metodo find"""
+"""mensaje="Esto es un mensaje de prueba para el metodo find"
+palabra_buscada="prueba"
+subcadena=mensaje.find(palabra_buscada)
+print("La posición de la palabra buscada "+palabra_buscada+" es: "+str(subcadena))"""
+
+"""Extracción de subcadena con posicion[inicial:final]"""
+"""mensaje="Esto es un mensaje de prueba para extrar una subcadena"
+posicion_inicial=22
+posicion_final=28
+extraer_subcadena=mensaje[posicion_inicial:posicion_final]
+print("la subcadena en la posicion es :"+extraer_subcadena)"""
+
+"""Comparación =="""
+mensaje_uno="Hola"
+mensaje_dos="Hola2"
+print(mensaje_uno==mensaje_dos)
+
+
